43c1170cd960ae42e87bdbfc5bbc87fc3903c721
s.milardo@hotmail.it <s.milardo@hotmail.it@3b5985e3-0193-c0c6-66f6-97e92c6a4aec>
2012-06-22 08:33:51 +0000

    Un commit che funonzia!!!

--- merendomatic/src/Gestore.java
+++ merendomatic/src/Gestore.java
@@ -1,7 +1,15 @@
+
+
 public class Gestore {
+	
+	Gestore(){
+		
+	}
 		
 	public static void main(final String args[]){
 		int x =6;
 		System.out.println(x);
+		System.out.println("Ufffiiiii");
+		System.out.println("WOW");
 	} 
 }
